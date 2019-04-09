f06c7bb6d402ceeed09d713c2bdbd487b07768a5
adan2 <44459607+adan2@users.noreply.github.com>
2019-04-09 15:32:18 -0700

    Site has moved

---
+++ docs/en/js/fuzzydata.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {indexDict['en'] = [{ "title" : "Our Documentation Has Moved", 
+"url" : "site-moved.html", 
+"breadcrumbs" : "sitemoved-rename-to-current-before-use \/ Our Documentation Has Moved", 
+"snippet" : "Please visit our new documentation site...", 
+"body" : "Please visit our new documentation site " }
+]
+$(document).trigger('search.ready');
+});
